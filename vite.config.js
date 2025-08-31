523627a62d4c42913eb327ae7b21882ec5779393
sridhar <sridhar.kesavan@outlook.com>
2025-08-31 14:13:07 +0900

    Cleanup for GitHub pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import ghPages from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), ghPages()],
+  plugins: [vue(), tailwindcss()],
     base: '/NoteMakingApp/', // Replace with your repo name
 })
